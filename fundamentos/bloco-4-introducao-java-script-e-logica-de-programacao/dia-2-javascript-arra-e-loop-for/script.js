feff527ea93c70ba4b284819111e769724ea0822
jmariomonteiro <mariomonteiro6@gmail.com>
2022-05-13 21:17:26 -0300

    Adicionei primeiro arquivo do dia

---
+++ fundamentos/bloco-4-introducao-java-script-e-logica-de-programacao/dia-2-javascript-arra-e-loop-for/script.js
@@ -0,0 +1,32 @@
+//fixação 1
+//let menuServices = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+
+//console.log("Menu de serviços: " + menuServices);
+
+//fixação 2
+
+//let indexOfPortfolio= ['Home', 'Serviços', 'Portfólio', 'Links'];
+//for (let index = 0; index < indexOfPortfolio.length; index += 1) {
+//    console.log(indexOfPortfolio[index]);
+//}
+
+//fixação 3 
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+// menu.push('Contato');
+// console.log(menu);
+
+// fixação 4
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// for ( let index = 0; index < groceryList.length; index +=1) {
+//     console.log(groceryList[index]);
+// }
+
+//fixação 5
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let pessoas of names){
+    console.log(pessoas);
+}
+
