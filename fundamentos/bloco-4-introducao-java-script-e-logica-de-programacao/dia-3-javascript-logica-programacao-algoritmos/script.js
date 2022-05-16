c233d1e4a3b59b1243674153bf2c0a8cd8339cc5
jmariomonteiro <mariomonteiro6@gmail.com>
2022-05-16 16:01:33 -0300

    Questão 1 e 2

---
+++ fundamentos/bloco-4-introducao-java-script-e-logica-de-programacao/dia-3-javascript-logica-programacao-algoritmos/script.js
@@ -0,0 +1,20 @@
+//questão 1
+
+// let fatorial = 10;
+// let resultado = fatorial
+
+// for (index = 1; index < fatorial; index +=1){
+//     resultado = index * resultado
+// }
+
+// console.log(resultado)
+
+//questão 2
+
+let word = "trybe";
+
+let reverterWord = '';
+
+reverterWord = word.split('').reverse().join('');
+
+console.log(reverterWord);
