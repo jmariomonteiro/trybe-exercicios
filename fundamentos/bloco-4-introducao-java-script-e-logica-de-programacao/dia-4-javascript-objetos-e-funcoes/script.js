13681f6c24e7b8c10ffa3c3a29e7de175d9016f3
jmariomonteiro <mariomonteiro6@gmail.com>
2022-05-17 16:39:58 -0300

    exercicios de fixação

---
+++ fundamentos/bloco-4-introducao-java-script-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -0,0 +1,46 @@
+// //fixação 1
+
+// let player = {
+//     name: 'Marta',
+//     lastName: 'Silva',
+//     age: 34,
+//     bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
+//     medals: { 
+//         golden: 2, 
+//         silver: 3,
+//     },
+// };
+
+// console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade.");
+// console.log("A jogadora " + player.name + " " + player.lastName + " foi eleita a melhor do mundo " + player.bestInTheWorld.length + " vezes");
+// console.log("A jogadora possui " + player.medals.golden + " medalhas de outro e " + player.medals.silver + " medalhas de prata");
+
+//fixação 2
+
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
+
+//   for (let index in names) {
+//       console.log("olá " + index, names[index]);
+//   } 
+
+// let car = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+//   };
+
+//   for (let index in car) {
+//       console.log(index, car[index])
+//   };
+
+function greetingsMessage(name) {
+    console.log("Boas vindas, " + name);
+  }
+  
+  
+  
+  
