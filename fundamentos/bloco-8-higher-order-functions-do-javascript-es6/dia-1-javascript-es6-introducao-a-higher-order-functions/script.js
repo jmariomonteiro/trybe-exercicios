906922b330f8bcf0fcc201df1db09fd57a14dfca
jmariomonteiro <mariomonteiro6@gmail.com>
2022-06-22 19:34:10 -0300

    Refazendo exercicio 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -12,6 +12,21 @@
 //     return employess
 // };
 
+//2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const sorteio = (numeroApostado, numeroAleatorio) => {
+    const sorteado = Math.floor(Math.random() * 5 + 1);
+    return resultado(numeroApostado, numeroAleatorio)
+};
+
+const resultado = (sorteado, callback) => {
+    if (sorteado === callback) {
+        return 'Parabéns, você foi escolhido!'
+    } else {
+        return 'Tente novamente!'
+    }
+}
+console.log(sorteio(2, resultado));
 // console.log(newEmployess(createEmployees));
 
 // const checkNumber = (myNumber, number2) => myNumber === number2
