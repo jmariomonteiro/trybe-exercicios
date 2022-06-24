befe20331b136d1d6e025186daab47ffec60f03a
jmariomonteiro <mariomonteiro6@gmail.com>
2022-06-24 18:06:23 -0300

    Restante dos exercicios 8.3

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
@@ -79,11 +79,46 @@ const books = [
 // o livro foi lançado.
 
 
-const arrayObjetos = books.map((book) => (
-    {
-        author: book.author.name,
-        age: book.releaseYear - book.author.birthYear,
-    }
-)).sort((a, b) => a.age - b.age)
+// const arrayObjetos = books.map((book) => (
+//     {
+//         author: book.author.name,
+//         age: book.releaseYear - book.author.birthYear,
+//     }
+// )).sort((a, b) => a.age - b.age)
 
-console.log(arrayObjetos)
+// console.log(arrayObjetos)
+
+// Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
+// Dica: use a função filter;
+
+// const genero = books.filter((objetos) => {
+//     return objetos.genre === 'Fantasia' || objetos.genre === 'Ficção Científica';
+// });
+
+// console.log(genero)4 - Crie um array ordenado pelos livros com mais
+//  de 60 anos de publicação e ordene-o pelo livro mais velho.
+// Dica: use as funções filter e sort.
+
+// const livros = books.filter((livro) => {
+//     if (2022 - livro.releaseYear > 60)
+//     return livro
+// }).sort((a, b) => a.releaseYear - b.releaseYear);
+
+// console.log(livros)
+
+//Crie um array em ordem alfabética 
+//apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
+
+// const nome = books.filter((genero) =>{
+//     if (genero.genre === 'Fantasia' || genero.genre === 'Ficção Científica')
+//     return genero
+// }).map((genero) => genero.author.name).sort();
+
+// console.log(nome);
+
+const livros = books.filter((livro) => {
+        if (2022 - livro.releaseYear > 60)
+        return livro
+    }).map((livro) => livro.name);
+
+    console.log(livros)
