a7a59f213a4ea91788126dd224ee74d4ac35f14f
jmariomonteiro <mariomonteiro6@gmail.com>
2022-07-07 19:17:22 -0300

    Adicionando arquivos dos exercicios

---
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/api.js
@@ -0,0 +1,5 @@
+const API_URL = `https://api.coincap.io/v2/assets`;
+
+const fetchMoedas = () => {
+    fetch(API_URL, )
+}
---
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/apiScript.js
@@ -0,0 +1,27 @@
+// apiScript.js
+// const API_URL = 'https://icanhazdadjoke.com/';
+
+// const fetchJoke = () => {
+//   const myObject = {
+//     method: 'GET',
+//     headers: { 'Accept': 'application/json' }
+//   };
+
+//   fetch(API_URL, myObject)
+//     .then(response => response.json())
+//     .then(data => console.log(data));
+// };
+
+// window.onload = () => fetchJoke();
+
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  fetch(API_URL, { headers: { Accept: 'application/json' } })
+    .then(response => response.json())
+    .then(data =>
+      document.getElementById('jokeContainer').innerText = data.joke
+    );
+}
+
+window.onload = () => fetchJoke();
