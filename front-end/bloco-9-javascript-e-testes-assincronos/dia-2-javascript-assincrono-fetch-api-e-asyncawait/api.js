0d5ec5c4dd4330a1c82400d93706874a996207c8
jmariomonteiro <mariomonteiro6@gmail.com>
2022-07-08 11:50:06 -0300

    Exercicios

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/api.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/api.js
@@ -1,5 +1,33 @@
-const API_URL = `https://api.coincap.io/v2/assets`;
+const fetchMoedas = async () => {
+    const url = `https://api.coincap.io/v2/assets`;
+    const coins = await fetch(url)
+    .then((response) => response.json())
+    .then(data => data.data)
+    .catch((error) => error.toString())
 
-const fetchMoedas = () => {
-    fetch(API_URL, )
+    return coins;
+};
+
+const setCoins = async () => {
+    const coins = await fetchMoedas()
+
+    const listCoins = document.getElementById('cripto');
+
+    coins
+    .filter((_,index) => index < 10)
+    .forEach(element => {
+        const newList = document.createElement('li');
+        const usdPrice = Number(element.priceUsd); 
+        
+        newList.innerText = `${element.name}(${element.symbol}): ${usdPrice.toFixed(2)}`
+
+        listCoins.appendChild(newList);
+    });
 }
+
+window.onload = () => setCoins();
+
+//{ headers: { Accept: 'application/json' } })
+  //  .then(response => response.json())
+    //.then(
+   // );
